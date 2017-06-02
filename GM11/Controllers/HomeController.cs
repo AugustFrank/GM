bdb85fc57dde3517cfaa9749bc308f5bf28c034b
KapL <kapalzx@gmail.com>
2017-06-02 11:59:37 +0200

    Changes in HomeController, Show all cars in a List

--- GM11/Controllers/HomeController.cs
+++ GM11/Controllers/HomeController.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GM011.Models;
+using GM11.Models;
 
 namespace GM11.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly GMContext _context;
+        private static List<Car> List = new List<Car>();
+
+        public HomeController(GMContext context)
+        {
+            this._context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+
+           
+
+            foreach(Car item in _context.Car.ToList())
+            {
+                List.Add(item);
+            }
+
+            //List.Add()
+
+            return View(List);
         }
 
         public IActionResult About()
